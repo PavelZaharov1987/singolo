328030fb7a233b61a15801da33959e91b0a62ace
PavelZakharov <pavelzakharov1987@gmail.com>
2020-03-18 09:33:10 +0300

    fix: fix menu, portfolio and form script

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
 window.onload = function() {
     addMenuLinkClickHandler();
-    addSliderClickHandler();
     addTabsClickHandler();
 }
 
 // navigation menu script
 
 const addMenuLinkClickHandler = () => {
-    document.querySelector('.navigation').addEventListener('click', (event) => {
-        if (event.target.classList.contains('navigation_link')) {
-            let clickedMenuLink = event.target;
-            removeSelectedMenuLinks();
-            selectClickedMenuLink(clickedMenuLink);
+    document.addEventListener('scroll', onScroll);
+}
+
+function onScroll(event) {
+    const currentPosition = window.scrollY;
+    const section = document.querySelectorAll('main section');
+    const links = document.querySelectorAll('.navigation .navigation_link');
+
+    section.forEach((el) => {
+        if (el.offsetTop - 95 <= currentPosition && (el.offsetTop - 95 + el.offsetHeight) > currentPosition) {
+            links.forEach((a) => {
+                a.classList.remove('navigation_active');
+                if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+                    a.classList.add('navigation_active');
+                }
+            })
         }
     })
 }
 
-const removeSelectedMenuLinks = () => {
-    let tabs = document.querySelectorAll('.navigation .navigation_link');
-    tabs.forEach(tab => {
-        tab.classList.remove('navigation_active');
-    })
-}
-
-const selectClickedMenuLink = (clickedMenuLink) => {
-    clickedMenuLink.classList.add('navigation_active');
-}
-
 // slider
 
-const next = document.querySelector('.slider-item:before');
-const prev = document.querySelector('.slider-item:after');
+var slideIndex = 1; // Индекс слайда по умолчанию
+showSlides(slideIndex);
 
+document.querySelector('.control.left').addEventListener('click', () => {
+    minusSlide();
+})
 
-const addSliderClickHandler = () => {
-    next.addEventListener('click', () => {
-        showSlides(slideIndex += 1);
-    });
+document.querySelector('.control.right').addEventListener('click', () => {
+    plusSlide();
+})
 
-    prev.addEventListener('click', () => {
-        showSlides(slideIndex -= 1);
-    })
+const plusSlide = () => {
+    showSlides(slideIndex += 1);
 }
 
-const showSlides = (count) => {
-    let slides = document.querySelectorAll('.slider-item');
-    if (count > slides.length) {
-        slideIndex = 1;
-    }
-    if (count < 1) {
-        slideIndex = slides.length;
-    }
-    slides.forEach(slide => {
-        slide.classList.add('slider-item_visible');
-    })
-    slides[slideIndex - 1].classList.remove('slider-item_visible');
+const minusSlide = () => {
+    showSlides(slideIndex -= 1);  
 }
+
+const currentSlide = (n) => {
+    showSlides(slideIndex = n); // Устанавливает текущий слайд
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slider-item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('slider-item_visible');
+    }
+    slides[slideIndex - 1].classList.add('slider-item_visible');
+}
+
+// const addSliderClickHandler = () => {
+ 
+// }
 
 // navigation portfolio tabs script
 
@@ -65,6 +77,16 @@ const addTabsClickHandler = () => {
             let clickedTab = event.target;
             removeSelectedTabs();
             selectClickedTab(clickedTab);
+            mixImages();
+        }
+    })
+    document.querySelector('.tab__content').addEventListener('click', (event) => {
+        if (event.target.classList.contains('tab__image')) {
+            let clickedImage = event.target;
+            document.querySelector('.tab__content').querySelectorAll('.tab__image').forEach(item => {
+                item.classList.remove('tab_active');
+            });
+            selectClickedImage(clickedImage);
         }
     })
 }
@@ -80,12 +102,26 @@ const selectClickedTab = (clickedTab) => {
     clickedTab.classList.add('tab-button_active');
 }
 
+const mixImages = () => {
+    let gallery = document.querySelector('.tab__content');
+    let pictures = Array.from(document.querySelectorAll('.tab__image'));
+    let newGallery = pictures.sort(function() {return Math.random() - 0.5});
+    newGallery.forEach(item=>gallery.append(item));
+    gallery.querySelectorAll('.tab__image').forEach(item => {
+        item.classList.remove('tab_active');
+    });
+}
+
+const selectClickedImage = (clickedImage) => {
+    clickedImage.classList.add('tab_active');
+}
+
 // feedback modal
 
-const button = document.querySelector('#btn');
+const form = document.querySelector('#form');
 const closeButton = document.querySelector('#close-btn');
 
-button.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const subject = document.querySelector('#subject').value.toString();
     const comment = document.querySelector('#comment').value.toString();
